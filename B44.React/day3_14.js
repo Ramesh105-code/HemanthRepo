817160ba4c39f1b2dace62ecb26374e6b84901cd
Ramesh105-code <sjramnandu126@gmail.com>
2025-04-10 16:49:53 +0530

    file

--- B44.React/day3_14.js
+++ B44.React/day3_14.js
@@ -1,12 +1,12 @@
 import React from "react";
-import ShoppingList from "./components/ShoppingList";
+import TodoList from "./components/TodoList";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="app">
-      <h1>Dynamic Shopping List with Quantities</h1>
-      <ShoppingList />
+      <h1>Multi-Feature To-Do List</h1>
+      <TodoList />
     </div>
   );
 };
